31323ab9e86821ea1a0d3f0ea2fc738e507cc0ea
谢玄 <xiexuan@xiexuandeMacBook-Air.local>
2026-02-27 23:41:57 +0800

    [fix](GUI): 前段显示页面调整。

--- js/xie-blog.js
+++ js/xie-blog.js
@@ -5,13 +5,13 @@
  */
 
  /*!
- * Hux Blog v1.6.0 (http://startbootstrap.com)
- * Copyright 2016 @huxpro
+ * Xie Blog v1.6.0 (http://startbootstrap.com)
+ * Copyright 2016 @xie96808
  * Licensed under Apache 2.0 
  */
 
 // Tooltip Init
-// Unuse by Hux since V1.6: Titles now display by default so there is no need for tooltip
+// Unuse by Xie since V1.6: Titles now display by default so there is no need for tooltip
 // $(function() {
 //     $("[data-toggle='tooltip']").tooltip();
 // });
@@ -19,7 +19,7 @@
 
 // make all images responsive
 /* 
- * Unuse by Hux
+ * Unuse by Xie
  * actually only Portfolio-Pages can't use it and only post-img need it.
  * so I modify the _layout/post and CSS to make post-img responsive!
  */
